bedcba8086bd98c70464fd8a0ed417586820e41a
Arthur Tkachenko <atherdon@gmail.com>
2018-05-05 13:43:30 +0300

    updates. i create a separated file for storing data

---
+++ src/assets/data/features.js
@@ -0,0 +1,15 @@
+import screen01 from '../images/screen01.png';
+import screen02 from '../images/screen02.png';
+import screen03 from '../images/screen03.png';
+
+const features = [
+
+  'Feature 1', 'Awesome feature', screen01,
+
+  'Feature 2', 'Great feature',   screen02,
+
+  'Feature 3', 'Check this out',  screen03
+
+]
+
+ export default features;
--- src/components/StoreFeatures.js
+++ src/components/StoreFeatures.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import Feature from './Feature';
 import FeatureImage from './FeatureImage';
-import screen01 from '../assets/images/screen01.png';
-import screen02 from '../assets/images/screen02.png';
-import screen03 from '../assets/images/screen03.png';
+// import screen01 from '../assets/images/screen01.png';
+// import screen02 from '../assets/images/screen02.png';
+// import screen03 from '../assets/images/screen03.png';
+
+import data from '../assets/data/features'
 
 let features = [];
-let data = [];
+// let data = [];
 
 class StoreFeatures extends Component {
 
-    setData() {
-        data.push('Feature 1', 'Awesome feature', screen01, 'Feature 2', 'Great feature', screen02, 'Feature 3', 'Check this out', screen03);
-    }
+    // setData() {
+    //     data.push('Feature 1', 'Awesome feature', screen01, 'Feature 2', 'Great feature', screen02, 'Feature 3', 'Check this out', screen03);
+    // }
 
     printData() {
 
@@ -25,7 +27,7 @@ class StoreFeatures extends Component {
 
             i += 3;
         }
-        
+
         return features;
     }
 
