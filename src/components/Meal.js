857b1c4c746de50e71594a8ce050840393886335
smob123 <smob123@hotmail.com>
2018-06-22 14:31:01 -0300

    used component will mount to load elements

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button, ListGroup, ListGroupItem, Form, Input, FormGroup, Label } from 'reactstrap';
+import { 
+    Container, 
+    Button, 
+    ListGroup, 
+    ListGroupItem, 
+    Form, 
+    Input, 
+    FormGroup, 
+    Label 
+} from 'reactstrap';
 
 const mealArray = [];
+const itemList = [];
 
 class Meal extends Component {
     state = {
@@ -13,24 +23,32 @@ class Meal extends Component {
         description: '',
         descriptions: [],
         step: '',
-        steps: []
+        steps: [],
+        ingredient: '',
+        ingredients: []
     };
+
+    componentWillMount() {
+        this.setState({names: [this.state.name], images: [this.state.image],
+            descriptios: [this.state.description], steps: [this.state.step], 
+            ingredients: [this.state.ingredient]});
+    }
 
     submitOutput() {
         this.setState({names: [...this.state.names, this.state.name],
             images: [...this.state.images, this.state.image],
             descriptions: [...this.state.descriptions, this.state.description],
-            steps: [...this.state.steps, this.state.step]});
+            steps: [...this.state.steps, this.state.step],
+            ingredients: [...this.state.ingredients, this.state.ingredient]});
 
         this.renderArray();
     }
 
     renderArray() {
-        let content = [this.state.name, this.state.image, this.state.description, this.state.step];
+        let content = [this.state.name, this.state.image, this.state.description, this.state.ingredient, this.state.step];
         let valid = true;
 
         for (let i = 0; i < content.length; i++) {
-            console.log(content[i]);
             if (content[i] === '') {
                 valid = false;
             }
@@ -41,7 +59,10 @@ class Meal extends Component {
                         <h2>{this.state.name}</h2>
                         <img src={this.state.image} alt="not found" />
                     
-                        <h3>Steps:</h3>
+                        <h3>Ingredients</h3>
+                        <p>{this.state.ingredient}</p>
+                    
+                        <h3>Description:</h3>
                         <p>{this.state.description}</p>
                         <dl>
                             <dt>Steps:</dt>
@@ -93,19 +114,25 @@ class Meal extends Component {
                                    onChange={e => this.setState({step: e.target.value})}/>
                         </FormGroup>
                 
+                        <FormGroup>
+                            <Label>
+                                Ingredients:
+                            </Label>
+                            <Input type="textarea" onChange={e => this.setState({ingredient: [e.target.value]})} />
+                        </FormGroup>
+                
                         <Button color="primary"
-                                onClick={() => this.submitOutput()}>
+                                onClick={() => this.submitOutput()} >
                             Add meal
                         </Button>
                     </Form>
-                
                     <ListGroup id="food-items">
                         {mealArray}
                     </ListGroup>
                 </Container>
-                                        );
+                                            );
+                                }
                             }
-                        }
-                        ;
+                            ;
 
-                        export default Meal;
+                            export default Meal;
